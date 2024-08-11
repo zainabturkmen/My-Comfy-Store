ef7ea7fd24f45897955318dfb34741281c48a361
zainabturkmen <zainabturkmen7@gmail.com>
2024-08-11 22:34:44 +0430

    adding update

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 
 const products_reducer = (state, action) => {
+    
   if (action.type === "SIDEBAR_OPEN") {
     return { ...state, isSidebarOpen: true };
-  }
+    }
+    throw new Error(`No Matching "${action.type}" - action type`);    
 
-  throw new Error(`No Matching "${action.type}" - action type`);
+
 };
 
 
