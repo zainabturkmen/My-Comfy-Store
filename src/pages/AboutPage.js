e5631784a8f2d5ad7b9944c4476a45cf370da7a6
zainabturkmen <zainabturkmen7@gmail.com>
2024-08-13 12:46:16 +0430

    adding update

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import aboutImage from "../assets/aboutIamge.png";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage jhdfsdf</div>
-  )
-}
+    <div>
+      <Wrapper className="wrapper">
+        <img src={aboutImage} alt="nice couch" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+       
+        </article>
+      </Wrapper>
+    </div>
+  );
+};
 
-export default AboutPage
+const Wrapper = styled.div`
+
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+`;
+
+export default AboutPage;
+
+
+
+
+
+  //  <p>
+  //    Comfy Sloth is your ultimate destination for creating cozy and stylish
+  //    living spaces. We specialize in offering a curated selection of
+  //    high-quality, comfortable furniture that blends seamlessly with any home
+  //    décor. Our mission is to bring comfort and elegance to every room, making
+  //    your home a place where you can truly relax and unwind.
+  //  </p>;
