85fd46b233d2695466e80114012f50d3ab007b9c
zainabturkmen <zainabturkmen7@gmail.com>
2024-09-12 09:22:25 +0430

    adding update

--- src/components/AmountButtons.js
+++ src/components/AmountButtons.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { FaMinus, FaPlus } from "react-icons/fa";
+import styled from "styled-components";
 
-const AmountButtons = () => {
+const AmountButtons = ({ amount, increase, descrease }) => {
   return (
-    <div>AmountButtons</div>
-  )
-}
+    <Wrapper>
+      <button type="button" className="amount-btn" onClick={descrease}>
+        <FaMinus />
+      </button>
+      <h2 className="amount">{amount}</h2>
+      <button type="button" className="amount-btn" onClick={increase}>
+        <FaPlus />
+      </button>
+    </Wrapper>
+  );
+};
 
-export default AmountButtons
+const Wrapper = styled.div``;
+
+export default AmountButtons;
