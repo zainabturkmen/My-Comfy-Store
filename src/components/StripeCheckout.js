5ad7200b59bbd1e7429cae78910be79c1a5dbe02
zainabturkmen <zainabturkmen7@gmail.com>
2024-09-17 21:29:36 +0430

    adding update

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -1,15 +1,30 @@
 import React from "react";
 import styled from "styled-components";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  CardElement,
+  useStripe,
+  useElements,
+  Elements,
+} from "@stripe/react-stripe-js";
+import axios from "axios";
+import { useCartContext } from "../context/cart_context";
+import { useUserContext } from "../context/user_context";
+import { formatPrice } from "../utils/helpers";
+import { useNavigate } from "react-router";
 
-// const CheckoutForm = () => {
-//     <h2>hello from stripe checkout</h2>
-// };
+const promis = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
+const CheckoutForm = () => {
+  return <h2>hello from stripe checkout</h2>;
+};
 
 const StripeCheckout = () => {
   return (
     <Wrapper>
-      {/* <CheckoutForm /> */}
-      <h2>hello from stripe checkout</h2>
+      <Elements stripe={promis}>
+        <CheckoutForm />
+      </Elements>
     </Wrapper>
   );
 };
