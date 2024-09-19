38993edadcb5a95a9dc478d39a3816deea61e21f
zainabturkmen <zainabturkmen7@gmail.com>
2024-09-19 14:18:27 +0430

    adding update

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -12,6 +12,7 @@ import { useCartContext } from "../context/cart_context";
 import { useUserContext } from "../context/user_context";
 import { formatPrice } from "../utils/helpers";
 import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 
 const promis = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
@@ -79,6 +80,13 @@ const CheckoutForm = () => {
           </div>
         )}
         {/* show a success message upon compeltion  */}
+        <p className={succeeded ? "result-message" : "result-message hidden"}>
+          payment succeeded see the result in your
+          <a href={`https://dashboard.stripe.com/test/payments`}>
+            Stripe dashboard
+          </a>
+          Refresh the page to pay again
+        </p>
       </form>
     </div>
   );
@@ -94,49 +102,64 @@ const StripeCheckout = () => {
   );
 };
 
-const Wrapper = styled.div`
-  .App {
-    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-    font-size: 16px;
-    -webkit-font-smoothing: antialiased;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    height: 100vh;
-    width: 100vw;
-  }
-
+const Wrapper = styled.section`
   form {
     width: 30vw;
-    min-width: 500px;
     align-self: center;
     box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1),
       0px 2px 5px 0px rgba(50, 50, 93, 0.1),
       0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
     border-radius: 7px;
     padding: 40px;
-    margin-top: auto;
-    margin-bottom: auto;
   }
-
-  #payment-message {
+  input {
+    border-radius: 6px;
+    margin-bottom: 6px;
+    padding: 12px;
+    border: 1px solid rgba(50, 50, 93, 0.1);
+    max-height: 44px;
+    font-size: 16px;
+    width: 100%;
+    background: white;
+    box-sizing: border-box;
+  }
+  .result-message {
+    line-height: 22px;
+    font-size: 16px;
+  }
+  .result-message a {
+    color: rgb(89, 111, 214);
+    font-weight: 600;
+    text-decoration: none;
+  }
+  .hidden {
+    display: none;
+  }
+  #card-error {
     color: rgb(105, 115, 134);
     font-size: 16px;
     line-height: 20px;
-    padding-top: 12px;
+    margin-top: 12px;
     text-align: center;
   }
-
-  #payment-element {
-    margin-bottom: 24px;
+  #card-element {
+    border-radius: 4px 4px 0 0;
+    padding: 12px;
+    border: 1px solid rgba(50, 50, 93, 0.1);
+    max-height: 44px;
+    width: 100%;
+    background: white;
+    box-sizing: border-box;
   }
-
+  #payment-request-button {
+    margin-bottom: 32px;
+  }
   /* Buttons and links */
   button {
-    background: #0055de;
+    background: #5469d4;
     font-family: Arial, sans-serif;
     color: #ffffff;
-    border-radius: 4px;
+    border-radius: 0 0 4px 4px;
     border: 0;
     padding: 12px 16px;
     font-size: 16px;
@@ -147,23 +170,19 @@ const Wrapper = styled.div`
     box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
     width: 100%;
   }
-
   button:hover {
     filter: contrast(115%);
   }
-
   button:disabled {
     opacity: 0.5;
     cursor: default;
   }
-
   /* spinner/processing state, errors */
   .spinner,
   .spinner:before,
   .spinner:after {
     border-radius: 50%;
   }
-
   .spinner {
     color: #ffffff;
     font-size: 22px;
@@ -177,17 +196,15 @@ const Wrapper = styled.div`
     -ms-transform: translateZ(0);
     transform: translateZ(0);
   }
-
   .spinner:before,
   .spinner:after {
     position: absolute;
     content: "";
   }
-
   .spinner:before {
     width: 10.4px;
     height: 20.4px;
-    background: #0055de;
+    background: #5469d4;
     border-radius: 20.4px 0 0 20.4px;
     top: -0.2px;
     left: -0.2px;
@@ -196,11 +213,10 @@ const Wrapper = styled.div`
     -webkit-animation: loading 2s infinite ease 1.5s;
     animation: loading 2s infinite ease 1.5s;
   }
-
   .spinner:after {
     width: 10.4px;
     height: 10.2px;
-    background: #0055de;
+    background: #5469d4;
     border-radius: 0 10.2px 10.2px 0;
     top: -0.1px;
     left: 10.2px;
@@ -209,117 +225,6 @@ const Wrapper = styled.div`
     -webkit-animation: loading 2s infinite ease;
     animation: loading 2s infinite ease;
   }
-
-  /* dynamic payment methods annotation */
-  #dpm-annotation {
-    align-self: center;
-    color: #353a44;
-    width: 30vw;
-    min-width: 500px;
-    line-height: 20px;
-    margin-bottom: 20px;
-  }
-
-  #dpm-integration-checker {
-    display: inline;
-    color: #533afd;
-  }
-
-  /* Payment status page */
-  #payment-status {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    row-gap: 30px;
-    width: 30vw;
-    min-width: 500px;
-    min-height: 380px;
-    align-self: center;
-    box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1),
-      0px 2px 5px 0px rgba(50, 50, 93, 0.1),
-      0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
-    border-radius: 7px;
-    padding: 40px;
-    opacity: 0;
-    animation: fadeInAnimation 1s ease forwards;
-    margin-top: auto;
-    margin-bottom: auto;
-  }
-
-  #status-icon {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 40px;
-    width: 40px;
-    border-radius: 50%;
-  }
-
-  h2 {
-    margin: 0;
-    color: #30313d;
-    text-align: center;
-  }
-
-  a {
-    text-decoration: none;
-    font-size: 16px;
-    font-weight: 600;
-    font-family: Arial, sans-serif;
-    display: block;
-  }
-  a:hover {
-    filter: contrast(120%);
-  }
-
-  #details-table {
-    overflow-x: auto;
-    width: 100%;
-  }
-
-  table {
-    width: 100%;
-    font-size: 14px;
-    border-collapse: collapse;
-  }
-  table tbody tr:first-child td {
-    border-top: 1px solid #e6e6e6; /* Top border */
-    padding-top: 10px;
-  }
-  table tbody tr:last-child td {
-    border-bottom: 1px solid #e6e6e6; /* Bottom border */
-  }
-  td {
-    padding-bottom: 10px;
-  }
-
-  .TableContent {
-    text-align: right;
-    color: #6d6e78;
-  }
-
-  .TableLabel {
-    font-weight: 600;
-    color: #30313d;
-  }
-
-  #view-details {
-    color: #0055de;
-  }
-
-  #retry-button {
-    text-align: center;
-    background: #0055de;
-    color: #ffffff;
-    border-radius: 4px;
-    border: 0;
-    padding: 12px 16px;
-    transition: all 0.2s ease;
-    box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
-    width: 100%;
-  }
-
   @keyframes loading {
     0% {
       -webkit-transform: rotate(0deg);
@@ -330,20 +235,10 @@ const Wrapper = styled.div`
       transform: rotate(360deg);
     }
   }
-  @keyframes fadeInAnimation {
-    to {
-      opacity: 1;
-    }
-  }
-
   @media only screen and (max-width: 600px) {
-    form,
-    #dpm-annotation,
-    #payment-status {
+    form {
       width: 80vw;
-      min-width: initial;
     }
   }
 `;
-
 export default StripeCheckout;
