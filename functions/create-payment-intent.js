56d0c7f7e86d622ecf6daedc3294b72129ee0418
zainabturkmen <zainabturkmen7@gmail.com>
2024-09-26 07:01:15 +0430

    adding update

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,11 +1,17 @@
 //domain/.netlify/functions/create-payment-intent
 
 exports.handler = async function (event, context) {
-  const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
-  console.log(cart);
+  if (event.body) {
+    const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
+    console.log(cart);
 
+    return {
+      statusCode: 200,
+      body: JSON.stringify(cart),
+    };
+  }
   return {
     statusCode: 200,
-    body: JSON.stringify(cart),
-  };
+    body:"Create Payment Intent"
+  }
 };
