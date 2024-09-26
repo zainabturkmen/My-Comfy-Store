ae43a2f8e41ea9766b8f14e1603a39960485faa8
zainabturkmen <zainabturkmen7@gmail.com>
2024-09-26 07:24:08 +0430

    adding update

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -8,8 +8,10 @@ exports.handler = async function (event, context) {
   if (event.body) {
     const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
 
-    
-    console.log(cart);
+
+    const calculateOrderAmount = () => {
+      
+    }
 
     return {
       statusCode: 200,
