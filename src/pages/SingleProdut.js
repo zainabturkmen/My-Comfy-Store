31cf75eeb4d46bd29221f294779a84dc693b7578
zainabturkmen <zainabturkmen7@gmail.com>
2024-10-04 22:54:49 +0430

    adding update

--- src/pages/SingleProdut.js
+++ src/pages/SingleProdut.js
@@ -24,18 +24,9 @@ const SingleProdut = () => {
     single_product: product,
     fetchSingleProduct,
   } = useProductsContext();
-  const {
-    name,
-    price,
-    description,
-    stock,
-    stars,
-    reviews,
-    id: sku,
-    company,
-    images,
-  } = product;
-  console.log(product);
+  const { name, price, description, stock, stars, reviews, id: sku, company, images, } = product;
+  
+  // console.log(product);
 
   useEffect(() => {
     fetchSingleProduct(`${url}${id}`);
